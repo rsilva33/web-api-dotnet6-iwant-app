5cede33b4d1811ffa994a0dddde5bb5f87fc115d
Ronaldo Silva <ronaldo.silva.jr96@gmail.com>
2022-05-19 10:39:50 -0300

    API Validations

--- src/Domain/Products/Category.cs
+++ src/Domain/Products/Category.cs
@@ -1,7 +1,39 @@
-﻿namespace IWantApp.Domain.Products;
+﻿using Flunt.Validations;
+
+namespace IWantApp.Domain.Products;
 
 public class Category : Entity
 {
-    public string Name { get; set; }
-    public bool Active { get; set; } = true;
+    public string Name { get; private set; }
+    public bool Active { get; private set; }
+
+    public Category(string name, string createdBy, string editedBy)
+    {
+        Name = name;
+        Active = true;
+        CreatedBy = createdBy;
+        EditedBy = editedBy;
+        CreatedOn = DateTime.Now;
+        EditedOn = DateTime.Now;
+
+        Validade();
+    }
+
+    public void EditInfo(string name, bool active)
+    {
+        Active = active;
+        Name = name;
+
+        Validade();
+    }
+
+    private void Validade()
+    {
+        var contract = new Contract<Category>()
+                    .IsNotNullOrEmpty(Name, "Name", "Nome é obrigatório!")
+                    .IsGreaterOrEqualsThan(Name, 3, "Name")
+                    .IsNotNullOrEmpty(CreatedBy, "CreateBy")
+                    .IsNotNullOrEmpty(EditedBy, "EditedBy");
+        AddNotifications(contract);
+    }
 }
--- src/Endpoints/Categories/CategoryPost.cs
+++ src/Endpoints/Categories/CategoryPost.cs
@@ -10,14 +10,13 @@ public class CategoryPost
     public static Delegate Handle => Action;
     public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
     {
-        var category = new Category
+        var category = new Category(categoryRequest.Name, "Test", "Test");
+
+        if(!category.IsValid)
         {
-            Name = categoryRequest.Name,
-            CreatedBy = "Test",
-            CreatedOn = DateTime.Now,
-            EditedBy = "Test",
-            EditedOn = DateTime.Now,
-        };
+           return Results.ValidationProblem(category.Notifications.ConvertToProblemDetails());
+        }
+
         context.Categories.Add(category);
         context.SaveChanges();
 
